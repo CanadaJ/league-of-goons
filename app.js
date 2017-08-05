ffea3dd3b309d08aa59e95bd690742718fbb5e23
Justin <jdcblades@gmail.com>
2017-08-05 10:42:20 -0700

    heroku port

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get('/admin', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port 3000');
 });
