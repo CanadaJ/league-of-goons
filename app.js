49665d9b41b618a85fbb1b238627157f0df69541
Justin <jdcblades@gmail.com>
2017-09-08 18:37:59 -0700

    moving requires and express-session

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var form = require('express-form');
-var logger = require('morgan');
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
 
@@ -39,14 +36,16 @@ passport.deserializeUser(function(user, cb) {
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(bodyParser());
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(express.session({ secret: 'fuck goodell' }));
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser'));
+app.use(require('body-parser'));
+app.use(require('express-session')({ secret: 'fuck goodell' }));
+
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.set('view engine', 'ejs');
 
 var teams = [];
 var teamPicks = [];
