6e876f771b5bda84c12d4628e8c3e70fc9c928e1
Justin <jdcblades@gmail.com>
2017-09-08 18:48:28 -0700

    ?

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.use(require('morgan')('combined'));
+app.use(require('morgan')('dev'));
 app.use(require('cookie-parser'));
 app.use(require('body-parser'));
 // app.use(require('express-session')({ secret: 'fuck goodell' }));
